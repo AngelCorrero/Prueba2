ce40796c7880a98b06393df62afa2fbbc8a343c5
alumno <alumno@PC-XX>
2019-02-05 14:51:15 +0100

    tercer commit

--- ED_T4_EJEMPLOGIT/src/ejemploGit/EjercicioGit.java
+++ ED_T4_EJEMPLOGIT/src/ejemploGit/EjercicioGit.java
@@ -5,10 +5,11 @@ public class EjercicioGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hello World");
+		System.out.println("Goodbye World");
 		
 		int i;
-
+		
+		i=0;
 	}
 
 }
